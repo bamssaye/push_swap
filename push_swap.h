401d256334709a8c1a6478b9700f5afb5a53056f
bamssaye <bamssaye@c4r10p5.1337.ma>
2024-02-23 11:18:25 +0100

    add

---
+++ push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 06:01:27 by bamssaye          #+#    #+#             */
+/*   Updated: 2024/02/21 06:04:33 by bamssaye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+
+typedef struct stack_all
+{
+    int stack;
+    struct stack_all *next; 
+} stack_info;
+ 
+ 
+
+#endif
